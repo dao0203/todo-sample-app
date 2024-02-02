bc684b9fa4fc472ad0def0d1edc161770ab47c48
dao0203 <youzaizuoteng872@gmail.com>
2024-02-02 11:11:20 +0900

    update gradle

--- core/repository/build.gradle.kts
+++ core/repository/build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.todo.android.library)
+    alias(libs.plugins.todo.android.hilt)
 }
 
 android {
@@ -34,7 +34,8 @@ android {
 }
 
 dependencies {
-
+    api(projects.core.model)
+    implementation(projects.core.local)
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
