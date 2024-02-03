3d10eacd4c0ead43aa9fa09aa06c7aa4cadf05b8
dao0203 <youzaizuoteng872@gmail.com>
2024-02-03 11:57:06 +0900

    add module

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -3,8 +3,9 @@ import com.example.todo_sample_app.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
-class AndroidApplicationConventionPlugin: Plugin<Project> {
+class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
@@ -15,6 +16,13 @@ class AndroidApplicationConventionPlugin: Plugin<Project> {
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
                 defaultConfig.targetSdk = 34
+            }
+
+            dependencies {
+                add("implementation", project(":core:repository"))
+                add("implementation", project(":core:model"))
+
+                add("implementation", project(":feature:todo"))
             }
         }
     }
