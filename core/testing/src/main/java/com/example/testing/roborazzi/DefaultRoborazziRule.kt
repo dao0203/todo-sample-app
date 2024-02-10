b87c9e7e01283384060e00d5a6c6083ab624e05f
dao0203 <youzaizuoteng872@gmail.com>
2024-02-11 01:40:57 +0900

    add DefaultRoborazziRule

---
+++ core/testing/src/main/java/com/example/testing/roborazzi/DefaultRoborazziRule.kt
@@ -0,0 +1,6 @@
+package com.example.testing.roborazzi
+
+import com.github.takahirom.roborazzi.RoborazziRule
+
+val DefaultRoborazziRule =
+    RoborazziRule(RoborazziRule.Options(outputDirectoryPath = "../../actual_images"))
