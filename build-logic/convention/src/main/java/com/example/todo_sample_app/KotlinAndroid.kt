fdb42f15e55814c9a3ef81ba700ba761975623d6
dao0203 <youzaizuoteng872@gmail.com>
2024-02-11 23:13:10 +0900

    add task

--- build-logic/convention/src/main/java/com/example/todo_sample_app/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/example/todo_sample_app/KotlinAndroid.kt
@@ -5,17 +5,23 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 
 internal fun Project.configureKotlinAndroid(
-    commonExtension: CommonExtension<*,*,*,*,*>
-){
+    commonExtension: CommonExtension<*, *, *, *, *>
+) {
     commonExtension.apply {
         compileSdk = 34
-        defaultConfig{
+        defaultConfig {
             minSdk = 33
         }
 
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
+        }
+
+        tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_17.toString()
+            }
         }
     }
 }
