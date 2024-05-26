0d3bb7c15fce46c82c18c7f38e6d315bdfea1d1f
dao0203 <yuyudevelop0203@gmail.com>
2024-05-27 02:30:19 +0900

    add kotlinx datetime

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -1,13 +1,15 @@
 import com.android.build.gradle.LibraryExtension
 import com.example.todo_sample_app.configureKotlinAndroid
+import com.example.todo_sample_app.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        with(target){
-            with(pluginManager){
+        with(target) {
+            with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
             }
@@ -15,6 +17,10 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
                 defaultConfig.targetSdk = 34
+
+                dependencies {
+                    add("implementation", libs.findLibrary("kotlinx-datetime").get())
+                }
             }
         }
     }
